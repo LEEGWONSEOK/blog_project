c15b28a5ab4b1645da069dad5bf0f4426b332ceb
leegwonseok <leestart1023@gmail.com>
2022-03-31 18:04:33 +0900

    Feat: 프로필 이미지 업로드(multer)구현

---
+++ api/users/controllers/postProfileImg.js
@@ -0,0 +1,35 @@
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const dir = '../../../public/profile';
+
+fs.readdir(dir, (error) => {
+  // uploads 폴더 없으면 생성
+  if (error) {
+    fs.mkdirSync(dir);
+  }
+})
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, dir);
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+const postProfileImg = (req, res) => {
+  console.log(req.file);
+  res.json({ url: `/${req.file.filename}` });
+};
+
+module.exports = {
+  postProfileImg,
+  upload
+};
